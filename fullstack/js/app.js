9e7c2c8083a153b34da647206a4cd7d7df8ed441
toheedakhtar <toheedak333@gmail.com>
2024-08-12 13:11:07 +0530

    main : restructure

--- fullstack/js/app.js
+++ fullstack/js/app.js
@@ -8,4 +8,18 @@ person.name = function(){
     return this.firstName + " " + this.lastName
 }
 
-console.log(person.name())
+
+// console.log(person.name())
+console.log('values : ', Object.values(person))
+console.log('enteries : ', Object.entries(person))
+console.log('JSON Stringfy : ', JSON.stringify(person))
+console.log(Date())
+console.log("---------------------------")
+
+// template string - backticks
+template_string_text = `this 'is' a "template" string "text"`
+console.log(template_string_text)
+
+// escape chars - \' -> ' , \" -> " , \\ -> \
+text = "We are \"Viking\" from the North"
+console.log(text)
