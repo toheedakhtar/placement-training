6659a7aa3e09d1e64244cb63e879e0773896525b
toheedakhtar <toheedak333@gmail.com>
2024-08-13 13:56:00 +0530

    js : datatypes

---
+++ fullstack/js/index.js
@@ -0,0 +1,6 @@
+console,log('Hello World')
+
+//  datatype -> Primitive , Reference
+
+// primitive -> string, number , boolean, undefined, symbol, bigInt
+// reference -> object, array, Date, null 
