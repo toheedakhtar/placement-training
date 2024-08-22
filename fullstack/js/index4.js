d15469de45fb27521cd55ddefd5dd80ce2cb2b78
toheedakhtar <toheedak333@gmail.com>
2024-08-22 13:35:31 +0530

    js: object methods

---
+++ fullstack/js/index4.js
@@ -0,0 +1,56 @@
+//  ------------------------------------------- arr methods ---------------------------------------------------------------------
+
+// filter
+// let arr = [10,20,30,40,50,60];
+
+// let filterdArr = arr.filter(item=>item > 20);
+// console.log(filterdArr)
+
+// reduce
+// let sum = arr.reduce((acc, curr) => {
+//     return acc + curr
+// }, 1000)
+// console.log(sum)
+
+// let founditem = arr.find((item) => item > 30);
+// console.log(founditem)
+
+// let founditem = arr.find((item) => item === 30);
+// console.log(founditem)
+
+// let founditemIndex = arr.findIndex((item) => item === 30);
+// console.log(founditemIndex)
+// let arr = [11,20,30,40,50,60];
+
+// let hasEven = arr.some((item) => item%2===0);
+// console.log(hasEven)
+
+// let everyEven = arr.every((item) => item%2===0);
+// console.log(everyEven)
+
+
+// ----------------------------------------------- object Methods ----------------------------------------------------------------------
+
+let obj = { 
+    firstName : "Toheed",
+    lastName : "Akhtar"
+}
+
+let keys = Object.keys(obj)
+console.log(keys)
+
+let values = Object.values(obj)
+console.log(values)
+
+let entries = Object.entries(obj)
+console.log(entries)
+
+
+
+
+
+
+
+
+
+
