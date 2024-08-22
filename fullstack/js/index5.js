5f0d34bfa4946c034a1dff08aca18fd23f941b41
toheedakhtar <toheedak333@gmail.com>
2024-08-22 14:56:45 +0530

    js : string, date, math methods

--- fullstack/js/index5.js
+++ fullstack/js/index5.js
@@ -1,43 +1,69 @@
 //  string methods
 // diff bw slice, splice and split is asked in interviews.
 
-let str = "Hello World";
-let char = str.charAt(6)
-console.log(char)
+// let str = "Hello World";
+// let char = str.charAt(6)
+// console.log(char)
 
-let newstr = str.concat(' Techno')
-console.log(newstr)
+// let newstr = str.concat(' Techno')
+// console.log(newstr)
 
-let hasStr = str.includes('world');
-console.log(hasStr)
+// let hasStr = str.includes('world');
+// console.log(hasStr)
 
 // let index = str.indexOf('ld');dfsklfjjfd
-let index = str.lastIndexOf('l');
-console.log(index)
+// let index = str.lastIndexOf('l');
+// console.log(index)
 
-let newStr = str.slice(-5);  // accepts -tive value also
-console.log(newStr)
+// let newStr = str.slice(-5);  // accepts -tive value also
+// console.log(newStr)
 
-let newStrsub = str.substring(4 , 8);
-console.log(newStrsub)
+// let newStrsub = str.substring(4 , 8);
+// console.log(newStrsub)
 
-let lcstr = str.toLowerCase()
-let ucstr = str.toUpperCase()
-console.log(lcstr, ucstr)
+// let lcstr = str.toLowerCase()
+// let ucstr = str.toUpperCase()
+// console.log(lcstr, ucstr)
 
-// split
-let splitstr = str.split("o")
-console.log(splitstr)
+// // split
+// let splitstr = str.split("o")
+// console.log(splitstr)
 
-let str1 = "     Hello World";
-let trimmedStr = str.trim();
-console.log(trimmedStr)
+// let str1 = "     Hello World";
+// let trimmedStr = str.trim();
+// console.log(trimmedStr)
 
-let strrplce = str.replace("World", "Universe")
-console.log(strrplce)
+// let strrplce = str.replace("World", "Universe")
+// console.log(strrplce)
 
-let strrpt = str.repeat(3)
-console.log(strrpt)
+// let strrpt = str.repeat(3)
+// console.log(strrpt)
 
 //  these above methods doesnot touch original methods.
+
+
+//  date methods
+
+let date = new Date();
+console.log(date)
+
+console.log(date.getFullYear())
+console.log(date.getSeconds())
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleTimeString())
+console.log(date.toLocaleDateString())
+
+// Math object
+
+let num = Math.random()     // range : 0 - 0.999999999....
+console.log(num)
+
+let flaotNum = 10.5
+console.log(Math.floor(flaotNum), Math.ceil(flaotNum), Math.round(flaotNum))
+console.log(Math.pow(2,5))  // 2**5
+console.log(Math.sqrt(16))
+
+
+
 
