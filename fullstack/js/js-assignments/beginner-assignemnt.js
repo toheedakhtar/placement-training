421e87574a8f4d9b8eb956fd40c5337693850c9a
toheedakhtar <toheedak333@gmail.com>
2024-08-23 11:42:39 +0530

    js : assignment

---
+++ fullstack/js/js-assignments/beginner-assignemnt.js
@@ -0,0 +1,56 @@
+// Beginner-Level JavaScript Assignment
+
+// ------------------- Question 1: String Concatenation ----------------------------
+// Task: Write a JavaScript program that takes a first name and a last name as input, concatenates them to form a full
+// name, and then prints the full name
+
+// let firstname = prompt('enter Firstname : ')
+// let lastname = prompt('enter lastname : ')
+// let fullName = firstname + " " + lastname
+// console.log(fullName)
+
+// ----------------------------- Question 2: If-Else Statements ---------------------------------
+// Task: Write a JavaScript program that checks if a number is positive, negative, or zero. Print 'Positive', 'Negative', or
+// 'Zero' accordingly.
+
+// let num = +prompt('Enter a number: ')
+// if (num%2 === 0 && num !== 0){
+//     console.log('number is even')
+// }
+// else if(num === 0){
+//     console.log('number is 0')
+// }
+// else{
+//     console.log('number is odd')
+// }
+
+
+// --------------------------- Question 3: Loops ------------------------------------------------
+// Task: Write a JavaScript program that uses a `for` loop to print the numbers from 1 to 10.
+
+// for(let i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+// ---------------------------- Question 4: Functions ----------------------------------------
+// Task: Write a JavaScript function called `greet` that takes a name as an argument and prints 'Hello, [name]!' to the
+// console. Then, call the function with a sample name.
+
+// function greet(name){
+//     console.log('Hello, ' + name)
+// }
+// greet('toheed')
+
+// ------------------------ Question 5: Arrays --------------------------------------------------------
+// Task: Write a JavaScript program that creates an array of 5 numbers, calculates the sum of all the numbers in the array,
+// and prints the result.
+
+// let arr = [1,2,3,4,5]
+// let calSum = (arr) =>{
+//     let sum = 0
+//     for(let i=0; i<arr.length; i++){
+//         sum += i
+//     }
+//     return sum
+// }
+// console.log("sum of array : " ,calSum(arr))
