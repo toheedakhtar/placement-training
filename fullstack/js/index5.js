c447e57b9c90a671078d40ba00cd001c4afe9441
toheedakhtar <toheedak333@gmail.com>
2024-08-23 09:26:13 +0530

    js/ss : date, string, obj methods; observation

--- fullstack/js/index5.js
+++ fullstack/js/index5.js
@@ -42,28 +42,70 @@
 //  these above methods doesnot touch original methods.
 
 
-//  date methods
+// ------------------------------ date methods ----------------------------------------
 
-let date = new Date();
-console.log(date)
+// let date = new Date();
+// console.log(date)
 
-console.log(date.getFullYear())
-console.log(date.getSeconds())
-console.log(date.toDateString())
-console.log(date.toTimeString())
-console.log(date.toLocaleTimeString())
-console.log(date.toLocaleDateString())
+// console.log(date.getFullYear())
+// console.log(date.getSeconds())
+// console.log(date.toDateString())
+// console.log(date.toTimeString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toLocaleDateString())
 
-// Math object
+// ------------------------------- Math object ----------------------------------------------
 
-let num = Math.random()     // range : 0 - 0.999999999....
-console.log(num)
+// let num = Math.random()     // range : 0 - 0.999999999....
+// console.log(num)
 
-let flaotNum = 10.5
-console.log(Math.floor(flaotNum), Math.ceil(flaotNum), Math.round(flaotNum))
-console.log(Math.pow(2,5))  // 2**5
-console.log(Math.sqrt(16))
+// let flaotNum = 10.5
+// console.log(Math.floor(flaotNum), Math.ceil(flaotNum), Math.round(flaotNum))
+// console.log(Math.pow(2,5))  // 2**5
+// console.log(Math.sqrt(16))
+// console.log(Math.min(10, 1, 200, 400, 4))
 
+// console.log("OTP:",  Math.round(Math.random() * 10000))
+
+
+// function otpGen(num){
+//     let otp_num = Math.round(num * 10000)
+//     if (toString(otp_num).length == 3){
+//         otp_num = Math.round(num * 100000)
+//     }
+//     console.log(otp_num)
+//     // let OTP = Math.round()
+// }
+
+// num = Math.random()
+// otpGen(num)
+
+// console.log("OTP:", 1000 + Math.round(Math.random() * 9000))
+
+
+
+// -------- destrcuting -----------------
+
+let arr = [10,20,30,40,50,60]
+
+const [n1 , ,n2, ...n3] = arr      // keyname should not matter
+console.log(n1, n2, n3)            // .... is rest operator
+console.log(arr)
+
+
+//  obj destructing
+
+let obj = {
+    firstName : 'Toheed',
+    lastName : 'Akhtar',
+    address : 'udaipur',
+    gender : 'Male'
+}
+
+gender = 'male'
+let {firstName, lastName, address, gender:a, age=20} = obj;       // keyname should be same as in object
+console.log(firstName, lastName, address, a)
+console.log(age)
 
 
 
