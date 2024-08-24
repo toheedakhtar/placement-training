f3224f08e12b771620af8e7a1dfa5493c81203e0
toheedakhtar <toheedak333@gmail.com>
2024-08-24 17:20:50 +0530

    js : renamed files

--- fullstack/js/datatype.js
+++ fullstack/js/datatype.js
@@ -9,30 +9,7 @@ console.log('Hello World')
 // var a = "I'll be an artificial intelligence legend";
 // console.log(a)
 
-function roughSizeOfObject(object) {
-    var objectList = [];
-    var stack = [object];
-    var bytes = 0;
 
-    while (stack.length) {
-        var value = stack.pop();
-
-        if (typeof value === 'boolean') {
-            bytes += 4;
-        } else if (typeof value === 'string') {
-            bytes += value.length * 2;
-        } else if (typeof value === 'number') {
-            bytes += 8;
-        } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
-            objectList.push(value);
-
-            for (var i in value) {
-                stack.push(value[i]);
-            }
-        }
-    }
-    return bytes;
-}
 var str = 'thsi sis string'
 console.log(typeof(str))
 
