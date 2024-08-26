198e8a158064705a7807f3fe2fd4f6569856a3c5
toheedakhtar <toheedak333@gmail.com>
2024-08-26 11:34:33 +0530

    js: promises

--- fullstack/js/async.js
+++ fullstack/js/async.js
@@ -10,33 +10,64 @@
 // console.log(a)
 // console.log(b)
 
-//  promises
 
-function step1(callback){
-    setTimeout(() => {
-        console.log("Step 1 completed");
-        callback();
-    }, 1000)
-}
+//  ----------------------- callbacks
+// function step1(callback){
+//     setTimeout(() => {
+//         console.log("Step 1 completed");
+//         callback();
+//     }, 1000)
+// }
 
-function step2(callback){
-    setTimeout(() => {
-        console.log("Step 2 completed");
-        callback();
-    }, 1000)
-}
+// function step2(callback){
+//     setTimeout(() => {
+//         console.log("Step 2 completed");
+//         callback();
+//     }, 1000)
+// }
 
-function step3(callback){
-    setTimeout(() => {
-        console.log("Step 3 completed");
-        callback();
-    }, 1000)
-}
+// function step3(callback){
+//     setTimeout(() => {
+//         console.log("Step 3 completed");
+//         callback();
+//     }, 1000)
+// }
 
-step1(()=>{
-    step2(() => {
-        step3(()=>{
-            console.log('All steps completed.')
-        })
+// // ------------ call back hell
+// step1(()=>{
+//     step2(() => {
+//         step3(()=>{
+//             console.log('All steps completed.')
+//         })
+//     })
+// })
+
+// --------------  PROMISES  ; launched in ES6 to tackle call back hell as it is complex , not scalable
+// - object which contains multiple values,
+// - either fullfillled or broken or in pending state,
+// - no data ingested - pending state
+// - gotten data - fulfilled state
+// - some problem while getting data - pending state
+// - also a constrcutor func
+
+function fetchData(){
+    return new Promise((resolve, reject) => {       // new keyword for constructor
+        setTimeout(()=>{
+            try {
+                let data = {
+                    firstName : 'Toheed',
+                    lastName : 'Akhtar',
+                };
+                return resolve(data);
+            }
+            catch(error){
+                reject(error);
+            }
+        }, 2000);
     })
-})
+}
+
+console.log(fetchData());
+
+
+
