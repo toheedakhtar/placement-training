097de5dd23579e98ce47d640af19d2bb9a126b4c
toheedakhtar <toheedak333@gmail.com>
2024-08-27 13:43:04 +0530

    js : async

--- fullstack/js/async.js
+++ fullstack/js/async.js
@@ -83,54 +83,76 @@
 
 
 //  fetchapi
-let fetchedDataObj;
+// let fetchedDataObj;
 
-function fetchData(){
-    let data = fetch('https://fakestoreapi.com/products');
-    console.log('Next step after fetching..!');         // this will run before fetch, this is bad!
-    return data;
+// function fetchData(){
+//     let data = fetch('https://fakestoreapi.com/products');
+//     console.log('Next step after fetching..!');         // this will run before fetch, this is bad!
+//     return data;
+// }
+
+// let fetchedData = fetchData();
+// console.log(fetchedData);   // a promise
+
+// fetchedData
+// .then((res)=>{
+//     // console.log(res.json())      
+//     return res.json();      // is a promise -> to pass to next .then 
+// })
+// .then((data) => {
+//     prodDiv = document.getElementById('products');
+//     cardDiv = document.createElement("div");
+    
+//     data.forEach(dataItem => {
+//     cardDiv = document.createElement("div")
+//     cardDiv.innerHTML = `
+//     <h1>${dataItem.title}</h1>
+//     <img src=${dataItem.image} width=300 height=300>
+//     <h2>${dataItem.price}</h2>
+//     <p>${dataItem.description}</p>
+//     <p>${dataItem.category}</p>
+//     <p>${dataItem.rating.rate}</p>`;
+    
+//     cardDiv.className= "cards";
+//     prodDiv.append(cardDiv);
+// });
+// })
+// .catch((err)=> {
+//     console.log(err)
+// })
+
+
+
+//  ---------------------------------------------------- ASYNC , AWAIT
+// async functon --> returns promise
+// async - await --> waits until the await line is executed before executing next
+
+async function fetchData() {    // asynchronous function
+    try {
+        let res = await fetch('https://fakestoreapi.com/products');     // awaits for completion before executing next lines.
+        let data = await res.json();
+        // return data;
+        document.getElementById('btn').addEventListener("click", function(){
+            data.forEach((dataitem)=>{
+                console.log(dataitem)
+            })
+        })
+    }
+    catch(error){
+        return error;
+    }
 }
+let data = fetchData();
+console.log(data);
 
-let fetchedData = fetchData();
-console.log(fetchedData);   // a promise
+// const fetchData = async () => {
+//     let res = await fetch('https://fakestoreapi.com/products');     // awaits for completion before executing next lines.
+//     let data = await res.json();
+//     console.log(data)
+//     return data;
+// }
 
-fetchedData
-.then((res)=>{
-    // console.log(res.json())  
-    return res.json();      // is a promise -> to pass to next .then 
-})
-.then((data) => {
-    prodDiv = document.getElementById('products');
-    cardDiv = document.createElement("div");
-    
-    data.forEach(dataItem => {
-    cardDiv = document.createElement("div")
-    cardDiv.innerHTML = `
-    <h1>${dataItem.title}</h1>
-    <img src=${dataItem.image} width=300 height=300>
-    <h2>${dataItem.price}</h2>
-    <p>${dataItem.description}</p>
-    <p>${dataItem.category}</p>
-    <p>${dataItem.rating.rate}</p>`;
-    
-    cardDiv.className= "cards";
-    prodDiv.append(cardDiv);
-
-});
-})
-.catch((err)=> {
-    console.log(err)
-})
-
-
-
-
-
-
-
-
-
-
+// console.log(fetchData)
 
 
 
