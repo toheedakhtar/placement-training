5d2fdcac9b915c5cc7950a791adbf06e07072b4b
toheedakhtar <toheedak333@gmail.com>
2024-09-19 14:02:17 +0530

    project : backend-userFound

--- fullstack/backend/controller/user.js
+++ fullstack/backend/controller/user.js
@@ -1,24 +1,45 @@
 const User = require("../model/user")
+
 exports.signup = async (req,res)=>{
-    try{
-       const {name, email , password , phoneNumber}=req.body;
-       const isExisting = await  UserActivation.findOne({email : email});
-       if(isExisting)
-       {
-        res.status(400).send({messeage : "user Already exit"});
-       }
-
-       const newUser = new User({name : name,email:email,password:password,phoneNumber:phoneNumber});
-
-       await newUser.save();
-       res.status(201).send({messege:"account created"});
-
-    }
-    catch(error){
-        if(error.name==="validationError"){
-            const errors = Object.values(error.errors).map(error=>error.messege);
-            res.status(400).json({messege:"validation Error",error :error})
+    try {
+        const { name , email , password , phoneNumber } = req.body;
+        const isExisting = await User.findOne({email : email});
+        if(isExisting){
+            return res.status(400).send({message  : "User Already Exist"});
         }
-      res.status(500).send(error);
+
+        const newUser = new User({name : name , email : email , password : password , phoneNumber  : phoneNumber});
+
+        await newUser.save();
+        res.status(201).send({message : "Account created"});
+    } catch (error) {
+        if(error.name === "ValidationError"){
+            const errors = Object.values(error.errors).map(error => error.message);
+            return res.status(400).json({message : "Validation Error",  error : errors});
+        }
+        res.status(500).send(error);
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const isExisting = await User.findOne({email : email});
+
+        if (!isExisting){
+            return res.status(400).send({message : "User not found"})
+        };
+
+        const isMatched = password === isExisting.password;
+
+        if (!isMatched){
+            return res.status(401).send({message : "Wrong Password"})
+        }
+
+        res.status(200).send({messafe : "User Logged In!", data: isExisting})
+
+
+    } catch (error) {
+        res.status(500).send({error});
     }
 }
--- fullstack/backend/index.js
+++ fullstack/backend/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const conncectDb = require('./config/db');
-const authRoutes = require('../backend/router/user')
-// const { connect } = require("mongoose");
-
-
+const connectDb = require("./config/db");
+const authRoutes = require("./router/user");
+ 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(authRoutes());
 
-conncectDb();
+app.use("/auth" , authRoutes);
+
+connectDb();
 
 app.listen(5000,()=>{
-    console.log("server is running on 5000");
-})
+    console.log("Server is running on 5000");
+});
