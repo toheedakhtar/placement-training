4ff6e4ce5c00efbeb753d2d8a0d4e3414b5290df
toheedakhtar <toheedak333@gmail.com>
2024-10-04 22:41:09 +0530

    Add Products

--- fullstack/project/backend/index.js
+++ fullstack/project/backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const connectDb = require("./config/db")
 const userRoutes = require('./router/user')
 const authRoutes = require('./router/auth')
+const productRoutes = require('./router/product')
 const errorHandler = require('./middleware/globalErrorHandler')
 const passport = require('passport')
 const session = require("express-session")
@@ -23,11 +24,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cors())
 app.use(passport.initialize());
+app.use(passport.session());
+
 connectDb();
 app.use('/uploads', express.static(path.join(__dirname, "uploads")))
 
 app.use('/auth', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use("/api", productRoutes)
 app.use(errorHandler);
 
 app.listen(5000,()=>{
